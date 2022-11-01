dde1f1ca93749acc50a9afa2692b2ba44842a11e
Tahlia <tbolden13@gmail.com>
2022-11-01 00:27:11 -0700

    staic middleware added

--- public/server.js
+++ public/server.js
@@ -11,6 +11,12 @@ const writeFileAsync = util.promisify(fs.writeFile);
 //settig up the server, make sure to communicate with Heroku an environment port or if that doesnt exist 5000, also set a variable (instance) in value of express//
 const app = express();
 const PORT = process.env.PORT || 5000;
-//specifies middleware for express to use
+//specifies middleware for express to use//
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json ());
+
+//static middleware for static files i.e. CSS
+app.use(express.static("./public"));
+
+//reading db json converting into an array and how to view each note: API Rounte Section//
+
