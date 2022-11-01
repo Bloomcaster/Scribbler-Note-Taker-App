f6f314ffb25c7acdbaa0249291cfcec4a5a4972f
Tahlia <tbolden13@gmail.com>
2022-10-31 19:06:14 -0700

    added notes to code for functionality

--- public/server.js
+++ public/server.js
@@ -8,3 +8,9 @@ const util = require('util');
 const readFileAsync = util.promisify(fs.readfile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+//settig up the server, make sure to communicate with Heroku an environment port or if that doesnt exist 5000, also set a variable (instance) in value of express//
+const app = express();
+const PORT = process.env.PORT || 5000;
+//specifies middleware for express to use
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json ());
